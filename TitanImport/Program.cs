bac24c4d64f928de40833720d36ec4bbaffdf7a9
Oliver Waits <owaits@gmail.com>
2020-06-23 20:34:25 +0100

    #1 added option to select the FPS when importing reaper CSV files. These will be imported either with the correct frame rate for Titan V13 and onwards or normalized to 100FPS for previous version before WebAPI supported FPS.

--- TitanImport/Program.cs
+++ TitanImport/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Blazor.FileReader;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace TitanImport
@@ -10,7 +12,7 @@ namespace TitanImport
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
             builder.RootComponents.Add<App>("app");
 
