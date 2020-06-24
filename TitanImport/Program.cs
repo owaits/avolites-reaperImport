9326b43875e5746ceb2a54e4e679a7b84b4f7dad
Oliver Waits <owaits@gmail.com>
2020-06-24 11:06:03 +0100

    Fixed issue where it would not read the file correctly when published to GitHub pages. Ensured that the first CueList is selected when refreshing.

--- TitanImport/Program.cs
+++ TitanImport/Program.cs
@@ -12,9 +12,9 @@ namespace TitanImport
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);            
 
             await builder.Build().RunAsync();
         }
